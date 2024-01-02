caa37d9279f2176d4d7699e06ae9cb48791f86c4
Bond_009 <bond.009@outlook.com>
2024-01-02 15:04:25 +0100

    Improve UI

--- app/src/main/java/com/example/androidweatherapplication/ui/overview/WeatherOverview.kt
+++ app/src/main/java/com/example/androidweatherapplication/ui/overview/WeatherOverview.kt
@@ -2,10 +2,13 @@ package com.example.androidweatherapplication.ui.overview
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
@@ -17,13 +20,13 @@ fun WeatherOverview(
 
     val weatherApiState = weatherOverviewViewModel.weatherApiState
 
-    Column() {
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Box(modifier = modifier) {
             when (weatherApiState) {
                 is WeatherApiState.Loading -> Text("Loading...")
                 is WeatherApiState.Error -> Text("Couldn't load...")
                 is WeatherApiState.Success -> WeatherComponent(
-                    weatherOverviewState = weatherOverviewState.value,
+                    weatherOverviewState = weatherOverviewState.value
                 )
             }
         }
@@ -35,16 +38,11 @@ fun WeatherComponent(
     modifier: Modifier = Modifier,
     weatherOverviewState: WeatherOverviewState
 ) {
-    val currentWeather = weatherOverviewState.currentWeather;
-    if (currentWeather == null) {
-        Text(text = "Nothing to see here")
-    }
-    else {
-        Column {
-            Text(text = currentWeather.name)
-            Text(text = String.format("%.0f°C", currentWeather.main.temp))
-            Text(text = String.format("Min %.0f°C Max %.0f°C", currentWeather.main.tempMin, currentWeather.main.tempMax))
-            Text(text = String.format("%d%% Humidity", currentWeather.main.humidity))
-        }
+    val currentWeather = weatherOverviewState.currentWeather!!;
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = currentWeather.name, fontSize = 32.sp)
+        Text(text = String.format("%.0f°C", currentWeather.main.temp), fontSize = 64.sp)
+        Text(text = currentWeather.weather[0].main)
+        Text(text = String.format("H:%.0f°C L:%.0f°C", currentWeather.main.tempMax, currentWeather.main.tempMin))
     }
 }
